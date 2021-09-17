29393cabfc33035d6d4c6683b1aa39ea86987955
Ochuko Ekrresa <ochuko@fliqpay.com>
2021-09-17 16:54:50 +0100

    Restructure fonts placement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,19 +10,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<link rel="icon" href="/favicon.ico" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap"
-					rel="stylesheet"
-				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
